bb9af2f4bd9e6f37c05ded63e0b9224565905bf2
Tim <harvard90873@gmail.com>
2021-07-17 16:37:05 +0200

    seems like everything is working
    gotta try it out first

--- server/dist/constants/email.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prefixMap = void 0;
-exports.prefixMap = {
-    confirm: "Y29uZmlybQ:",
-    "forgot-password": "Zm9yZ290:",
-};
-//# sourceMappingURL=email.js.map
--- server/ormconfig.js
+++ server/ormconfig.js
@@ -5,17 +5,42 @@ const fileType = MODE === "production" ? "js" : "ts";
 
 const production = MODE === "production";
 
-module.exports = {
+const DB_URl = process.env.DATABASE_URL;
+
+const parseHeroku = (url) => {
+  url = url.slice(11);
+
+  const [username, a, b] = url.split(":");
+
+  const [password, host] = a.split("@");
+  const [port, database] = b.split("/");
+
+  const res = {
+    host,
+    port,
+    username,
+    password,
+    database,
+  };
+
+  return res;
+};
+
+const {
+  username: HP_USERNAME,
+  password: HP_PASSWORD,
+  database: HP_DATABASE,
+  host: HP_HOST,
+  port: HP_PORT,
+} = parseHeroku(DB_URl);
+
+const config = {
   type: "postgres",
-  host: production ? process.env.HP_HOST : "localhost",
-  port: 5432,
-  username: production
-    ? process.env.HP_USERNAME
-    : process.env.POSTGRES_USERNAME,
-  password: production
-    ? process.env.HP_PASSWORD
-    : process.env.POSTGRES_PASSWORD,
-  database: production ? process.env.HP_DATABASE : "auth-api-template",
+  host: production ? HP_HOST : "localhost",
+  port: production ? parseInt(HP_PORT) : 5432,
+  username: production ? HP_USERNAME : process.env.POSTGRES_USERNAME,
+  password: production ? HP_PASSWORD : process.env.POSTGRES_PASSWORD,
+  database: production ? HP_DATABASE : "auth-api-template",
   entities: [`${basedir}/entity/**/*.${fileType}`],
   migrations: [`${basedir}/migrations/**/*.${fileType}`],
   subscribers: [`${basedir}/subscriber/**/*.${fileType}`],
@@ -34,3 +59,7 @@ module.exports = {
       }
     : undefined,
 };
+
+console.log(config);
+
+module.exports = config;
