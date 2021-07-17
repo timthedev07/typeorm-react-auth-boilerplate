fe73c56828477f1fecb8c746a2efa78aa4277d4f
Tim <harvard90873@gmail.com>
2021-07-17 22:32:48 +0200

    production frontend url

--- server/src/constants/global.ts
+++ server/src/constants/global.ts
@@ -1,7 +1,7 @@
 export const production = process.env.NODE_ENV === "production";
 
 export const FRONTEND = production
-  ? "https://your-site-name.netlify.app" // change this line
+  ? "https://typeorm-react-auth-boilerplate.netlify.app" // change this line
   : "http://localhost:3000";
 export const PLAYGROUND = "https://studio.apollographql.com";
 export const PORT = parseInt(process.env.PORT || "9000");
